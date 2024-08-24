0fc1ef00d99906aebb9d61d51a6ecf48e812c70e
Thanarit Kanjanametawat <u6410322@au.edu>
2024-08-24 18:20:01 +0700

    Fixed When Deployment Data not Loaded

--- caranalytic-react/src/pages/DashboardPage.jsx
+++ caranalytic-react/src/pages/DashboardPage.jsx
@@ -3,23 +3,10 @@ import { Pie, Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, ArcElement, BarElement, CategoryScale, LinearScale, Tooltip, Legend } from 'chart.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './DashboardPage.css';
+import carDataOG from '/src/assets/taladrod-cars.json';
 
 // Registering necessary Chart.js components
 ChartJS.register(ArcElement, BarElement, CategoryScale, LinearScale, Tooltip, Legend);
-
-
-
-// Function to generate a random color
-const getRandomColor = () => {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-};
-
-
 
 
 // DashboardPage component
@@ -39,27 +26,24 @@ const DashboardPage = () => {
   
 };
 
+
 useEffect(() => {
-    fetch('/src/assets/taladrod-cars.json')
-      .then(response => response.json())
-      .then(data => {
-        const processedData = processCarData(data.Cars);
-        const fullProcessedData = processFullCarData(data.Cars); // Process full data
+  const processedData = processCarData(carDataOG.Cars);
+  const fullProcessedData = processFullCarData(carDataOG.Cars); // Process full data
 
-        setCarData(processedData);
-        setFullCarData(fullProcessedData); // Set full data
+  setCarData(processedData);
+  setFullCarData(fullProcessedData); // Set full data
 
-        generateColorMap(fullProcessedData.brands);
+  generateColorMap(fullProcessedData.brands);
 
-        // Initialize all brands as collapsed
-        const initialCollapseState = Object.keys(fullProcessedData.brands).reduce((acc, brand) => {
-          acc[brand] = true; // true means collapsed
-          return acc;
-        }, {});
-        setCollapsedBrands(initialCollapseState);
-      })
-      .catch(error => console.error('Error loading the car data:', error));
-  }, []);
+  // Initialize all brands as collapsed
+  const initialCollapseState = Object.keys(fullProcessedData.brands).reduce((acc, brand) => {
+    acc[brand] = true; // true means collapsed
+    return acc;
+  }, {});
+  setCollapsedBrands(initialCollapseState);
+}, []);
+
 
 
 
